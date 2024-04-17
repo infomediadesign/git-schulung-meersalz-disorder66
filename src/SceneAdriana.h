fd2d6cbe3ac3f288a8c6daef7aa44d7525106cad
AdrianaLucena <adrianalucena2002@gmail.com>
2024-04-17 14:58:23 +0200

    added scene

--- src/SceneAdriana.h
+++ src/SceneAdriana.h
@@ -2,12 +2,29 @@
 // Created by adria on 17.04.2024.
 //
 
+#include <vector>
+#include "iostream"
+#include "raylib.h"
+
 #ifndef RAYLIBSTARTER_SCENEADRIANA_H
 #define RAYLIBSTARTER_SCENEADRIANA_H
 
+class GameObject;
 
 class SceneAdriana {
+public:
+    void update();
+    void draw();
+    SceneAdriana(const char* _filename);
 
+    // Collision detection functions
+    bool touchesWall(Vector2 _pos, Vector2 _size);
+    bool touchesWall(Vector2 _pos);
+
+
+private:
+    std::vector<GameObject*> objects;
+    std::vector<int> tilemap;
 };
 
 
