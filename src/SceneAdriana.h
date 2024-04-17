d2d404ae9fd0de72e76334b012be7a068134bdbd
AdrianaLucena <adrianalucena2002@gmail.com>
2024-04-17 15:17:39 +0200

    scene stuff

--- src/SceneAdriana.h
+++ src/SceneAdriana.h
@@ -3,6 +3,7 @@
 //
 
 #include <vector>
+#include <cstdint>
 #include "iostream"
 #include "raylib.h"
 
@@ -10,21 +11,34 @@
 #define RAYLIBSTARTER_SCENEADRIANA_H
 
 class GameObject;
+class Particle;
 
 class SceneAdriana {
 public:
-    void update();
+    void update(); // first, update all objects, then delete those marked for deletion
     void draw();
     SceneAdriana(const char* _filename);
 
     // Collision detection functions
     bool touchesWall(Vector2 _pos, Vector2 _size);
     bool touchesWall(Vector2 _pos);
+    Vector2 touchWallPosition(Vector2 _pos, Vector2 _size);
+    Rectangle getWallArt(Vector2 _pos, Vector2 _size);
+    std::vector<GameObject*> getGameObjectsAt(Vector2 _pos, Vector2 _size);
+    // game object handling functions
+    void addGameObject(GameObject* _object);
+    GameObject* getGameObject(int64_t _id);
+    //particle handling functions
+
 
 
 private:
     std::vector<GameObject*> objects;
     std::vector<int> tilemap;
+    std::vector<Particle*> particles;
+
+    int mapWidth;
+    int mapHeight;
 };
 
 
