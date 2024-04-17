9473ead0dfa1f46b1bb4aa22f3bb031ccf287640
AdrianaLucena <adrianalucena2002@gmail.com>
2024-04-17 14:45:18 +0200

    Changed Text pos

---
+++ src/SceneAdriana.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adria on 17.04.2024.
+//
+
+#include "SceneAdriana.h"
---
+++ src/SceneAdriana.h
@@ -0,0 +1,14 @@
+//
+// Created by adria on 17.04.2024.
+//
+
+#ifndef RAYLIBSTARTER_SCENEADRIANA_H
+#define RAYLIBSTARTER_SCENEADRIANA_H
+
+
+class SceneAdriana {
+
+};
+
+
+#endif //RAYLIBSTARTER_SCENEADRIANA_H
